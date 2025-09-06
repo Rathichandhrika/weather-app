58c8b89ad14fa6e35933b56b9c84cd94a29f7317
Rathichandhrika S <chandhrikarathi09@gmail.com>
2025-09-06 16:15:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ async function getWeather() {
   if (data.error) {
     document.getElementById("result").innerHTML = `<p>❌ ${data.error}</p>`;
   } else {
+    const icon = `https://openweathermap.org/img/wn/10d@2x.png`;
     document.getElementById("result").innerHTML = `
       <h3>${data.city}</h3>
+      <img src="${icon}" alt="weather icon" />
       <p>🌡️ Temperature: ${data.temp}°C</p>
       <p>💧 Humidity: ${data.humidity}%</p>
       <p>☁️ Condition: ${data.description}</p>
